3790e32be4a30d5f09f4bee8d386cbb8841668a0
Evgeniy <75693065+Tomato-angel@users.noreply.github.com>
2025-02-11 18:13:26 +0300

    Create project files

---
+++ SeaBattle/Assets/_Main/Scripts/Debug/MatchMaker.cs
@@ -0,0 +1,220 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Linq.Expressions;
+using Mirror;
+using System.Threading.Tasks;
+using System.Data;
+using Unity.VisualScripting;
+
+public class MatchMaker
+{
+    #region [Реализация синглтона]
+    private static MatchMaker _instance;
+    public static MatchMaker Instance { 
+        get{
+            if (_instance == null)
+            {
+                _instance = new MatchMaker();
+            }
+            return _instance;
+        } 
+    }
+    #endregion
+
+    #region [Реализация работы с единым хранилищем матчей]
+
+    private int _maxMatchPlayers = 2;
+    public int MaxMatchPlayers { get => _maxMatchPlayers; }
+
+    private int _matchesLimitCount;
+    public int MatchesLimitCount { get => _matchesLimitCount; }
+    
+
+    private Dictionary<string, Match> _matches;
+    public Dictionary<string, Match> Matches { get => _matches; }
+
+    // Дополнить везде Callback.Invoke() нужно будет тут написать возможность заранее брать из класса коды коллбеков
+
+    private bool CreateMatch(out string key, out Match match, bool isOpen = false, Action<Callback> callback = null)
+    {
+        key = string.Empty;
+        match = Match.Empty;
+        if (_matches.Count < _matchesLimitCount)
+        {
+            string tmpKey = string.Empty;
+            if(KeyGenerator.Instance.TryGenerateKey(out tmpKey))
+            {
+                key = tmpKey;
+                Match tmpMatch = new Match(tmpKey, isOpen);
+                match = tmpMatch;
+                _matches.Add(tmpKey, tmpMatch);
+                Debug.Log($"-- Created game with key: {tmpKey}");
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private bool DeleteMatch(string key, Action<Callback> callback = null)
+    {
+        if(_matches.ContainsKey(key))
+        {
+            KeyGenerator.Instance.RemoveKey(key);
+            _matches.Remove(key);
+            return true;
+        }
+        return false;
+    }
+
+    private void RemoveEmptyMatches()
+    {
+        foreach((string key, Match match) in _matches)
+        {
+            if(match.PlayersCount == 0)
+            {
+                DeleteMatch(key);
+            }
+        }
+    }
+
+    private bool ConnectPlayerToMatch(string key, Player player, Action<Callback> callback = null)
+    {
+        Debug.Log($"-- Starts connect to game with key: {key} the Player: {player.connectionToClient.connectionId}");
+        if(_matches.ContainsKey(key))
+        {
+            if (_matches[key].PlayersCount < _maxMatchPlayers)
+            {
+                _matches[key].AddPlayer(player);
+
+                if (_matches[key].PlayersCount == _maxMatchPlayers)
+                {
+                    _matches[key].IsFull = true;
+                }
+
+                player.CurrentMatch = _matches[key];
+                player.NetworkMatch.matchId = _matches[key].ID;
+
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private bool DisconnectPlayerFromMatch(string key, Player player, Action<Callback> callback = null)
+    {
+        Debug.Log($"-- Starts disconnect game with key: {key} the Player: {player.connectionToClient.connectionId}");
+        if (_matches.ContainsKey(key))
+        {
+            if (_matches[key].ContainsPlayer(player))
+            {
+                _matches[key].DeletePlayer(player);
+                if (_matches[key].PlayersCount == 0)
+                {
+                    DeleteMatch(key);
+                }
+
+                player.CurrentMatch = null;
+                player.NetworkMatch.matchId = Guid.Empty;
+
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private bool ForceDisconnectPlayerFromMatch(Player player, Action<Callback> callback = null)
+    {
+        player.CurrentMatch = null;
+        player.NetworkMatch.matchId = Guid.Empty;
+
+        return true;
+    }
+
+    // Изменить все сигнатуры методов void на bool
+    // Реализовать некоторый PlayerNotifier который будет перелавать на клиент данные об ошибке с сервера о его операции
+    public bool HostGame(Player player, Action<Callback> callback = null)
+    {
+        Debug.Log($"- Player connId: {player.connectionToClient.connectionId}, starts host game");
+
+        string key;
+        Match match;
+        if (CreateMatch(out key, out match))
+        {
+            if(ConnectPlayerToMatch(key, player, callback))
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public bool JoinGame(Player player, string key, Action<Callback> callback = null)
+    {
+        Debug.Log($"- Player connId: {player.connectionToClient.connectionId}, starts join game");
+        if(ConnectPlayerToMatch(key, player, callback))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool SearchGame(Player player, Action<Callback> callback = null)
+    {
+        Debug.Log($"- Player connId: {player.connectionToClient.connectionId}, starts search game");
+
+        Debug.Log($"-- Try connect to opens matches");
+        foreach ((string tmpKey, Match tmpMatch) in _matches)
+        {
+            if (tmpMatch.IsOpen)
+            {
+                if (tmpMatch.PlayersCount < _maxMatchPlayers)
+                {
+                    if(ConnectPlayerToMatch(tmpKey, player, callback))
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+        Debug.Log($"-- Try create the open matches");
+        string key;
+        Match match;
+        if (CreateMatch(out key, out match))
+        {
+            if (ConnectPlayerToMatch(key, player, callback))
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public bool LeaveGame(Player player, string key, Action<Callback> callback = null)
+    {
+        Debug.Log($"- Player connId: {player.connectionToClient.connectionId}, leave the game from match key: {key}");
+        if(DisconnectPlayerFromMatch(key, player, callback))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    #endregion
+
+    public MatchMaker(int maxMatchPlayers = 2, int matchesLimitCount = 10)
+    {
+        _maxMatchPlayers = maxMatchPlayers;
+        _matchesLimitCount = matchesLimitCount;
+        _matches = new Dictionary<string, Match>();
+    }
+
+}
---
+++ SeaBattle/Assets/_Main/Scripts/Debug/Player.cs
@@ -0,0 +1,110 @@
+using Mirror;
+using System;
+using System.Text.RegularExpressions;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkMatch))]
+[RequireComponent(typeof(NetworkIdentity))]
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] string _nickName;
+
+    #region [Объявление Mirror - компонентов]
+    private NetworkMatch _networkMatch;
+    public NetworkMatch NetworkMatch { get => _networkMatch; }
+
+    private NetworkIdentity _networkIdentity;
+    public NetworkIdentity NetworkIdentity { get => _networkIdentity; }
+    #endregion
+
+    private Match _currentMatch;
+    public Match CurrentMatch { get => _currentMatch; set => _currentMatch = value; }
+
+    
+    public void Awake()
+    {
+        _networkMatch = GetComponent<NetworkMatch>();
+        _networkIdentity = GetComponent<NetworkIdentity>();
+    }
+
+    public void Start() 
+    {
+        // CLIENT
+        if(isClient)
+        {
+            CmdSearchGame();
+            CmdLeaveGame();
+            CmdHostGame();
+            CmdLeaveGame();
+            CmdJoinGame("XXXXxxxx");
+        }
+
+        // SERVER
+        if(isServer)
+        {
+
+        }
+    }
+
+    public void Update()
+    {
+        // CLIENT
+        if (isClient)
+        {
+
+        }
+
+        // SERVER
+        if (isServer)
+        {
+
+        }
+    }
+
+    #region ["Player" на стороне клиента]
+
+    [TargetRpc]
+    public void TargetViewMatchInfo(NetworkConnectionToClient target, string matchInfo)
+    {
+        if(matchInfo == null) 
+            Debug.Log("No information about match");
+        else
+            Debug.Log(matchInfo);
+    }
+
+    #endregion
+
+    #region ["Player" на стороне сервера]
+
+    [Command]
+    public void CmdSearchGame()
+    {
+        MatchMaker.Instance.SearchGame(this);
+        TargetViewMatchInfo(connectionToClient, _currentMatch?.ToString());
+    }
+
+    [Command]
+    public void CmdHostGame()
+    {
+        MatchMaker.Instance.HostGame(this);
+        TargetViewMatchInfo(connectionToClient, _currentMatch?.ToString());
+    }
+
+    [Command]
+    public void CmdJoinGame(string key)
+    {
+        MatchMaker.Instance.JoinGame(this, key);
+        TargetViewMatchInfo(connectionToClient, _currentMatch?.ToString());
+    }
+
+    [Command]
+    public void CmdLeaveGame()
+    {
+        MatchMaker.Instance.LeaveGame(this, _currentMatch.Key);
+        TargetViewMatchInfo(connectionToClient, _currentMatch?.ToString());
+    }
+
+    #endregion
+}
